44555638bc46ebf292227321a7c1400840f5f532
Con <62197403+inacceptable@users.noreply.github.com>
2022-03-15 11:03:40 +1100

    mm

--- toolkit/settings.py
+++ toolkit/settings.py
@@ -121,6 +121,10 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
+MEDIA_URL = '/media/'
+
+# Path where media is stored
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
