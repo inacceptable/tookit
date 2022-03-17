e538d362b9dff13a2c8ddec6fcd48eb5f171da6b
Con <62197403+inacceptable@users.noreply.github.com>
2022-03-17 17:13:26 +1100

    sdf

--- main/admin.py
+++ main/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import txt_to_pdf, feedback
+from .models import txt_to_pdf, feedback, docx_to_pdf
 
 admin.site.register(txt_to_pdf)
 admin.site.register(feedback)
+admin.site.register(docx_to_pdf)
