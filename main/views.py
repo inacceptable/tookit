b9d2eb296f0d15c5cadddaa33d5c49b4ed6ca377
Con <62197403+inacceptable@users.noreply.github.com>
2022-03-18 15:51:15 +1100

    kkj

--- main/views.py
+++ main/views.py
@@ -119,9 +119,8 @@ def docx_to_pdf_converter(request):
 	txt_path = path+new_object.docx_file.url
 	pdf_c_path = txt_path.replace(".docx", ".pdf")
 	convert(txt_path, pdf_c_path)	
-	pdf_path = pdf_path.replace(".docx", ".pdf")
-	new_object.pdf_file = pdf_path
-	pdf_path = "http://toolkit-website.herokuapp.com" + new_object.docx_file.url
+	new_object.pdf_file = pdf_c_path
+	pdf_path = "http://127.0.0.1:8000" + new_object.pdf_file.url
 	print(pdf_path)
 	new_object.save()
 	data = {
